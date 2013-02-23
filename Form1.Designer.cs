2708a078b9f40083aca5a12523096c0777ffc35e
ruby <ruby092977@gmail.com>
2013-02-23 18:28:14 +0800

    implement
    user can auto download from everbox after login

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace EverBoxTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1040, 585);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(446, 244);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterExpand);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get Root";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(498, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(568, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Current Path";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Scan";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(336, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Go Up";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(417, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Home";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(3, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(446, 244);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.WordWrap = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(255, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Stop";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 41);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1054, 617);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1046, 591);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Browser";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.treeView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(452, 250);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "TreeView";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.textBox2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(452, 250);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "LinksOutput";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 670);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace EverBoxTool
+{
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    public partial class Form1 : Form
+    {
+        private static int Concurrency = 5;
+        private System.Threading.Semaphore semaphore = new System.Threading.Semaphore(Concurrency, Concurrency);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.ObjectForScripting = this;
+            this.webBrowser1.Navigate(@"http://www.everbox.com/f/" + this.linkID);
+        }
+
+        private string linkID = "mKqJ211fRAFY183t9RYOEXMzDp";
+        private HtmlElement fileViewer;
+        private Timer scanTimer;
+        private string outputPath = @"G:\outputLinks.txt";
+        private string downloadPath = @"G:\ClassicMusic\";
+        private int ProcessCount = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.treeView1.Nodes.Clear();
+            this.fileViewer = this.webBrowser1.Document.GetElementById("fileviewer");
+            TreeNode tn = new TreeNode(this.getCurrentPath());
+            tn.Name = tn.Text;
+            tn.Nodes.Add("Loading...");
+            this.treeView1.Nodes.Add(tn);
+            //if (File.Exists(this.outputPath)) File.Delete(this.outputPath);
+        }
+
+        public void newFileLink(string fileName, string fileLink)
+        {
+            //MessageBox.Show(fileName + "||||" + fileLink);
+            string extension = Path.GetExtension(Path.GetFileName(fileName));
+
+            this.textBox2.AppendText(fileName + "\r\n");
+            this.textBox2.AppendText(fileLink + "\r\n");
+            
+            FileStream fs = new FileStream(this.outputPath, FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+            sw.WriteLine(fileName);
+            sw.WriteLine(fileLink);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+
+            return;
+            
+            fileName = fileName.Replace(" ", "");
+            string directory = fileName.Substring(0, fileName.LastIndexOf(@"/"));
+            if (!Directory.Exists(downloadPath + directory))
+            {
+                Directory.CreateDirectory(downloadPath + directory);
+            }
+            if (File.Exists(downloadPath + fileName))
+            {
+                return;
+            }
+            semaphore.WaitOne();
+            Process p = new Process();
+            Console.WriteLine(fileName);
+            p.StartInfo.WorkingDirectory = downloadPath;
+            p.StartInfo.FileName = @"C:\Users\Ruby\Desktop\wget-1.11.4-1-bin\bin\wget.exe";
+            p.StartInfo.Arguments = string.Format("{0} -O {1}", fileLink, downloadPath + fileName);
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = false;
+            p.EnableRaisingEvents = true;
+            p.Start();
+            p.Exited += (a, b) => { semaphore.Release(1); };
+                
+        }
+
+        private string getCurrentPath()
+        {
+            try
+            {
+                string result = this.webBrowser1.Document.GetElementById("back2_btn").GetAttribute("data-folder");
+                if (result == "") return @"/" + this.webBrowser1.Document.GetElementById("folder_title").InnerText;
+                else return result;
+            }
+            catch { return ""; }
+        }
+
+        private void goUpFolder()
+        {
+            this.webBrowser1.Document.GetElementById("back2_btn").InvokeMember("click");
+        }
+
+        private void treeView1_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Name == this.getCurrentPath())
+            {
+                //MessageBox.Show(e.Node.FullPath);
+                if (e.Node.Nodes.Count == 1 && e.Node.Nodes[0].Text == "Loading...")
+                {
+                    e.Node.Nodes.Clear();
+                    foreach (HtmlElement trnode in
+                        this.webBrowser1.Document.GetElementById("filelinker").Children[0].Children[0].Children)
+                    {
+                        TreeNode tn = new TreeNode(trnode.Id);
+                        tn.Name = tn.Text;
+                        if (this.webBrowser1.Document.GetElementById(tn.Name).GetElementsByTagName("span")[0].Children[0].GetAttribute("className").Contains("folder"))
+                        {
+                            tn.Nodes.Add("Loading...");
+                        }
+                        else
+                        {
+                            string path = tn.Name;
+                            path = path.Substring(path.IndexOf("/") + 1);
+                            path = path.Substring(path.IndexOf("/") + 1);
+                            object[] args = { path };
+                            this.webBrowser1.Document.InvokeScript("myfunc2", args);
+                        }
+                        if ((tn.Name != e.Node.Name) && (tn.Name.StartsWith(e.Node.Name)))
+                        {
+                            e.Node.Nodes.Add(tn);
+                        }
+                        else
+                        {
+                            e.Node.Nodes.Add("Loading...");
+                            e.Node.Collapse();
+                            return;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                /*
+                while (!e.Node.Name.StartsWith(this.getCurrentPath()))
+                {
+                    this.goUpFolder();
+                }
+                if (e.Node.Name != this.getCurrentPath())
+                {
+                    string subname = e.Node.Name.Substring(this.getCurrentPath().Length + 1);
+                    if (subname.Contains("/"))
+                    {
+                        subname = subname.Substring(0, subname.IndexOf("/"));
+                    }
+                    subname = this.getCurrentPath() + "/" + subname;
+                    object[] args = { subname };
+                */
+                object[] args = { e.Node.Name };
+                this.webBrowser1.Document.InvokeScript("myfunc", args);
+                e.Node.Collapse();
+                /*}*/
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = this.getCurrentPath();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.scanTimer = new Timer();
+            this.scanTimer.Interval = 500;
+            this.scanTimer.Tick += scanTimer_Tick;
+            this.scanTimer.Start();
+        }
+
+        string lastExpand = "";
+        int failCount = 0;
+
+        bool expandFirstNode(TreeNode tn)
+        {
+            if (tn.IsExpanded == false && tn.Nodes.Count > 0)
+            {
+                tn.Expand();
+                if (lastExpand == tn.Name)
+                {
+                    failCount++;
+                    if (failCount > 15)
+                    {
+
+                    }
+                }
+                else
+                {
+                    lastExpand = tn.Name;
+                    failCount = 0;
+                }
+                return true;
+            }
+            for (int i = 0; i < tn.Nodes.Count; i++)
+            {
+                if (expandFirstNode(tn.Nodes[i])) return true;
+            }
+            return false;
+        }
+
+        bool canScan()
+        {
+            HtmlElement flash = this.webBrowser1.Document.GetElementById("flash");
+            if (flash.Style == null) return true;
+            if (flash.Style.Contains("none")) return true;
+            if (flash.InnerText.Contains("加载中")) return false;
+            else return true;
+        }
+
+        void scanTimer_Tick(object sender, EventArgs e)
+        {
+            if (!canScan()) return;
+            if (!expandFirstNode(this.treeView1.Nodes[0])) this.scanTimer.Stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.goUpFolder();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            HtmlElement he = this.webBrowser1.Document.CreateElement("script");
+            he.SetAttribute("type", "text/javascript");
+            he.SetAttribute("text", @"function myfunc(arg){everbox.linkViewer.setPath(arg);}");
+            this.webBrowser1.Document.Body.AppendChild(he);
+
+            HtmlElement he2 = this.webBrowser1.Document.CreateElement("script");
+            he2.SetAttribute("type", "text/javascript");
+            he2.SetAttribute("text", @"function myfunc2(arg){
+                var n = {};
+                n[everbox.csrfKey] = everbox.csrfVal;
+                n.path = arg;
+                $.ajax({
+                    type: ""POST"",
+                    url: ""/f/download/"" + everbox.linkViewer.linkId,
+                    data: n,
+                    dataType: ""json"",
+                    success: function (c) {
+                        var a = c.code;
+                        LinkViewer_alertErrMsg(a);
+                        if (a == 200) {
+                            //window.location.href = c.data.dataurl
+                            window.external.newFileLink(n.path, c.data.dataurl);
+                        } else {
+                            myfunc2(arg);
+                        }
+                    }
+                })
+            }");
+            this.webBrowser1.Document.Body.AppendChild(he2);
+
+            //this.webBrowser1.Navigating += webBrowser1_Navigating;
+        }
+
+        void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            //MessageBox.Show(e.Url.ToString());
+            //e.Cancel = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Navigate(@"http://www.everbox.com/f/" + this.linkID);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (this.scanTimer != null) this.scanTimer.Stop();
+        }
+
+    }
+}
